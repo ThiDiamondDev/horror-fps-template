27e0a81edb290310e667939e4792f168bacbdd89
ThiDiamondDev <thi.dimond1@gmail.com>
2024-04-24 13:49:18 -0300

    Remove use item function from base class, so the value will be null if not defined and the button will be disabled.

--- inventory-interaction-system/Interaction/Items/CollectibleItem.gd
+++ inventory-interaction-system/Interaction/Items/CollectibleItem.gd
@@ -5,11 +5,10 @@ class_name CollectibleItem
 @export var item: ItemResource
 	
 func _ready():
-	item.use_item_function = get_script().use_item
-	
-static func use_item():
-	pass
-	
+	var script: Script = get_script()
+	if script.has_method("use_item"):
+		item.use_item_function = get_script().use_item
+
 func interact(_parameters=null):
 	Inventory.collect.emit(item)
 	queue_free()
